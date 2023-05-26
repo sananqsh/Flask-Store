54881ee4d08ccb1618e64b6d17c305b19fd5c32b
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-05-26 18:57:40 +0330

    Add put item

--- code/app.py
+++ code/app.py
@@ -33,6 +33,19 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
     
         return {'message': 'Item deleted'}
+    
+    @jwt_required()
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        if item:
+            item.update(data)
+        else:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+
+        return item
 
 class ItemList(Resource):
     def get(self):
