6eb1e3cf819ee219e7ab9338a0f555d6f4954aba
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-05-28 14:47:42 +0330

    Add SQLAlchemy for ItemList

--- code/resources/item.py
+++ code/resources/item.py
@@ -67,15 +67,5 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-        
-        return {'items': items}
+        result = ItemModel.all()
+        return {'items': [item.json() for item in result]}
